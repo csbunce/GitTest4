d4985a66877d39c7bffe7f20d6f240920060a22e
csbunce <sbuncemain@gmail.com>
2018-05-22 15:07:54 -0400

    Add feature 2.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,7 +13,10 @@ namespace ConsoleApp1
             // Code was edited in github
             // Code was added in VS
             // Code to call Feature 1
-            // Added code to Feature1 locally only.
+            // Added code to Feature1 locally only.'''
+            
+            // Add code to call Feature 2
+
         }
     }
 }
